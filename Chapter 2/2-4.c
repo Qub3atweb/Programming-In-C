c2efc2e1738e90c0ddc1e4bc4ce361844721ebaf
Christopher <christopher@cjwp.se>
2017-09-02 12:41:13 +0200

    Exercise 2.4

---
+++ Chapter 2/2-4.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#define SLUTSUMMA 1000000.0
+int main()
+{
+    int dagar = 0;
+    float sum = 0;
+    float lon = 0.01;
+while (sum < SLUTSUMMA)
+{
+    sum = sum + lon;
+    lon = 2*lon;
+    dagar++;
+}
+printf("Han måste jobba %d dagar för att nå %0.f\n", dagar, SLUTSUMMA);
+}
